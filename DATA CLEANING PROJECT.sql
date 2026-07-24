82bbb6aa555702bbd3e6f015beac97e61dfd6954
Akshat Pandey <techclashofficial@gmail.com>
2026-07-24 19:42:54 +0530

    Cleaning A Fully Fledged Dataset

--- DATA CLEANING PROJECT.sql
+++ DATA CLEANING PROJECT.sql
@@ -1,3 +1,10 @@
+-- 1. Remove Duplicates
+-- 2. Standardization
+-- 3. Null values or Blank Values 
+-- 4 Remove Any Column if not Needed 
+
+
+
 select * from layoffs;
 
 create table layoff_staging like layoffs;
@@ -16,6 +23,7 @@ from layoff_staging
 )
 select* from CTE_Clean where row_num >1;  
 
+-- DATA CLEANING
 CREATE TABLE `layoff_staging2` (
   `company` text,
   `location` text,
@@ -76,3 +84,52 @@ select distinct country , trim(trailing '.' from country);
 update layoff_staging2
 set country = trim(trailing  '.' from country)
 where country  like 'United States%'; 
+
+
+update layoff_staging2
+set `date` = str_to_date(`date` , '%m/%d/%Y');
+
+select * from layoxff_staging2;
+
+
+ALTER TABLE layoff_staging2
+MODIFY COLUMN `date` DATE;
+
+-- Handling Null values 
+select * from layoff_staging2
+where total_laid_off  IS NULL
+AND percentage_laid_off is NULL;
+
+
+select * from layoff_staging2 
+where industry is NULL
+OR industry = '';
+
+Update layoff_staging2
+set industry = NULL 
+where industry = '';
+
+
+select * from  layoff_staging2 t2
+JOIN layoff_staging2 t2
+	 ON t1.company = t2.company
+where(t1.industry is NULL or t1. industry = '')
+AND t2.industry is NOT NULL;
+
+update layoff_staging2 t1
+join layoff_staging2 t2
+ON t1.company = t2.company
+set t1.industry = t2.industry 
+where(t1.industry is NULL)
+AND t2.industry is NOT NULL;
+
+select * from layoff_staging2
+where total_laid_off is NULL
+AND percentage_laid_off is NULL;
+
+delete from layoff_staging2
+where total_laid_off is NULL
+AND percentage_laid_off is NULL;
+
+ALTER Table layoff_staging2
+DROP Column row_num;
